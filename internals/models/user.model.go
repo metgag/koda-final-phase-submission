f04170b58690d103cf027906e6a7adbdd6b66d4c
metgag <moreehigh1@gmail.com>
2025-10-01 14:17:06 +0700

    feat(user): add new model at user model model list: - post, to create a new user's post - comment post body, to create post comment

--- internals/models/user.model.go
+++ internals/models/user.model.go
@@ -1,8 +1,24 @@
 package models
 
-import "mime/multipart"
+import (
+	"mime/multipart"
+	"time"
+)
 
 type PostBody struct {
 	Content *string               `form:"content"`
 	Image   *multipart.FileHeader `form:"image"`
+}
+
+type Post struct {
+	PostID       int       `json:"post_id"`
+	Content      *string   `json:"content"`
+	Image        *string   `json:"image"`
+	CreatorID    int       `json:"creator_id"`
+	CreatorUname string    `json:"creator_uname"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type CommentPostBody struct {
+	Comment string `json:"comment"`
 }
